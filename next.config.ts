1a1a1e2a0c12ca9baceefb13f69acf3631126d38
Dikshit07 <dikshit.201747@ncit.edu.np>
2026-08-08 01:39:06 +0545

    updated image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
+   images: {
+
+    remotePatterns: [
+
+      {
+
+        protocol: "https",
+
+        hostname: "uptechsolution.com.np",
+
+        pathname: "/api/v1/backend/wp-content/uploads/**",
+
+      },
+
+    ],
+
+  },
 };
 
 export default nextConfig;
